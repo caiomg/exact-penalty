c5bbcf795cf0a23cc41c463915fd3314da90880f
Caio <caio@giuliani.eng.br>
2017-09-07 13:34:51 -0400

    Changed tests
    Now with more constraints and different algorithm parameters

--- test_l1.m
+++ test_l1.m
@@ -1,4 +1,4 @@
-dim = 2;
+dim = 7;
 
 % Objective
 Hf = 2*eye(dim);
@@ -25,30 +25,44 @@ gc = -ones(dim, 1);
 cc = 3^2 - 0.5;
 g3 = @(x) quadratic(Hc, gc, cc, x);
 
+all_con = cell(dim, 1);
+for k = 1:dim
+    Hc = zeros(dim);
+    Hc(k, k) = -2;
+    gc = zeros(dim, 1);
+    gc(k) = 1;
+    cc = 1^2 - 0.25;
+    gk = @(x) quadratic(Hc, gc, cc, x);
+    all_con{k} = gk;
+end
+
+
 % Initial point
 x0 = -2*ones(dim, 1);
+% x0 = -(1+3*sqrt(2))/2*ones(dim, 1);
+% x0 = -1.5*ones(dim, 1);
+% x0 = (1:dim)';
 
 % Parameters
 mu = 1e-1;
-epsilon = 1e-1;
+epsilon = 2;
 delta = 1e-6;
 Lambda = 0.1;
 
-nlcon = @(x) constraints({ @(y) -g2(y), @(y) -g3(y)}, {}, x);
+nlcon = @(x) constraints(all_con, {}, x, -1);
 fmincon_options = optimoptions(@fmincon, 'Display', 'off');
 x_fmincon = fmincon(f, x0,[],[],[],[],[],[], nlcon, fmincon_options)
-p = @(x) l1_function(f, {g2}, mu, x);
-x2_fmincon = fmincon(p, x0,[],[],[],[],[],[], nlcon, fmincon_options)
+p = @(x) l1_function(f, all_con, mu, x);
+%x2_fmincon = fmincon(p, x0,[],[],[],[],[],[], nlcon, fmincon_options)
 
 %%
-x = l1_penalty(f, {g2, g3}, x0, mu, epsilon, delta, Lambda)
-tl1 = @() l1_penalty(f, {g2, g, g3}, x0, mu, epsilon, delta, Lambda);
+x = l1_penalty(f, all_con, x0, mu, epsilon, delta, Lambda)
 
 
+tl1 = @() l1_penalty(f, all_con, x0, mu, epsilon, delta, Lambda);
 tmlab = @() fmincon(f, x0,[],[],[],[],[],[], nlcon, fmincon_options);
-
-% timeit(tl1)
-% timeit(tmlab)
+timeit(tl1)
+timeit(tmlab)
 
 
 
