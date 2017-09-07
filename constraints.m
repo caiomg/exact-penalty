377c7ea22093c2eb69ce41b94fc26073b6720ea3
Caio <caio@giuliani.eng.br>
2017-09-07 13:11:04 -0400

    Now function constraints has factor parameter
    Can be used for g(x) >= 0 or g(x) <= 0 constraints, if factor is 1 or -1

--- constraints.m
+++ constraints.m
@@ -1,15 +1,19 @@
-function [c, ceq] = constraints(c_in, ceq, x)
+function [c, ceq] = constraints(c_in, ceq, x, factor)
+
+if nargin < 4
+    factor = 1;
+end
 
 n_in = length(c_in);
 c = zeros(n_in, 1);
 for k = 1:n_in
-    c(k) = c_in{k}(x);
+    c(k) = factor*c_in{k}(x);
 end
 
 n_eq = length(ceq);
 ceq = zeros(n_eq, 1);
 for k = 1:n_eq
-    c(k) = ceq{k}(x);
+    c(k) = factor*ceq{k}(x);
 end
 
 end
