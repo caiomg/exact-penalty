aafa9e29bfc849bba9a163f16c63906592951cb4
Caio <caio@giuliani.eng.br>
2018-02-23 10:36:44 -0300

    l1_function now also returns function values
    Not only the value of p(x) is returned. As second return value, a list
    of the function values used to calculate p(x) is returned.

--- l1_function.m
+++ l1_function.m
@@ -1,4 +1,4 @@
-function p = l1_function(f, phi, mu, x, ind_eactive)
+function [p, fvalues] = l1_function(f, phi, mu, x, ind_eactive)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,14 +6,18 @@ if nargin < 5
     ind_eactive = [];
 end
 
-n_constraints = length(phi);
-sum_phi = 0;
-for n = 1:n_constraints
+n_functions = 1 + length(phi);
+fvalues = zeros(n_functions, 1);
+value_used = false(n_functions, 1);
+fvalues(1) = f(x);
+for n = 1:n_functions-1
+    fvalues(n+1, 1) = phi{n}(x);
     if isempty(find(ind_eactive == n, 1))
-        sum_phi = sum_phi + max(phi{n}(x), 0);
+        value_used(n+1) = true;
     end
 end
-p = f(x) + mu*sum_phi;
+sum_phi = sum(max(fvalues(value_used), 0));
+p = fvalues(1) + mu*sum_phi;
 
 end
 
