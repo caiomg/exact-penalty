fa939932caeaf85b780a59554883e9480da6c0a6
Caio <caio@giuliani.eng.br>
2018-02-23 10:20:22 -0300

    Changed way of getting TR model information
    Now the matrices c, g, H are returned directly

--- tr_modeling/get_model_matrices.m
+++ tr_modeling/get_model_matrices.m
@@ -1,20 +1,20 @@
-function p = get_model_polynomial(model, m)
+function [c, g, H] = get_model_matrices(model, m)
 %GET_MODEL_POLYNOMIAL Summary of this function goes here
 %   Detailed explanation goes here
 
 
-if m == 1
+if m == 0
     poly = model.model_polynomial;
 else
-    poly = model.other_polynomials{m - 1};
+    poly = model.other_polynomials{m};
 end
 
 [c, g, H] = coefficients_to_matrices(poly.dimension, poly.coefficients);
 
-p.c = c;
-p.g = g/model.scale_factor_x;
-p.H = H/(model.scale_factor_x)^2;
 
+% Scale
+g = g/model.scale_factor_x;
+H = H/(model.scale_factor_x)^2;
 
 end
 
