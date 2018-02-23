9be910c14c1781e521a724ff9e6c3d2c9824a311
Caio <caio@giuliani.eng.br>
2018-02-23 10:21:04 -0300

    Added function to verify interpolation

---
+++ tr_modeling/check_interpolation.m
@@ -0,0 +1,24 @@
+function max_diff = check_interpolation(model)
+
+% Remove center from all points
+h = model.points;
+n_points = size(model.points, 2);
+for m = n_points:-1:1
+    h(:, m) = h(:, m) - model.points(:, 1);
+end
+
+max_diff = -1;
+n_functions = size(model.fvalues, 1);
+for k = 1:n_functions
+    [c, g, H] = get_model_matrices(model, k-1);
+    for m = 1:n_points
+        this_value = c + g'*h(:, m) + 0.5*(h(:, m)'*H*h(:, m));
+        difference = abs(model.fvalues(k, m) - this_value);
+        if difference > max_diff
+            max_diff = difference;
+        end
+        if abs(difference) > 10*eps
+            error('cmg:tr_interpolation_error', 'Interpolation error');
+        end
+    end
+end
