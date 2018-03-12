4d0731c3655501cff85697e57e2d0f93e7a9702d
Caio <caio@giuliani.eng.br>
2018-03-12 07:59:20 -0300

    Added count of function evaluations
    Some other changes to test cases

--- test_cutest.m
+++ test_cutest.m
@@ -4,16 +4,18 @@ clear global problem_path_cutest problem_name_cutest problem_data_cutest
 global problem_data_cutest
 
 problem_name = 'HS100';
-% problem_name = 'WOMFLET';
+problem_name = 'WOMFLET';
 % problem_name = 'POLAK1';
 % problem_name = 'SNAKE';
 % problem_name = 'POLAK3'; % Second derivative of cons. too much important
 % problem_name = 'ZY2';
+problem_name = 'HS88';
 prob = setup_cutest_problem(problem_name, '../my_problems/');
-prob.equatn
 
 % Objective
-f = @(x) get_cutest_objective(x);
+f_obj = @(x) get_cutest_objective(x);
+counter = evaluation_counter(f_obj);
+f = @(x) counter.evaluate(x);
 
 % Constraints
 n_constraints = get_cutest_total_number_of_constraints();
@@ -27,7 +29,7 @@ end
 x0 = prob.x;
 
 % Parameters
-mu = 40;
+mu = 100;
 if strcmp(problem_name, 'SNAKE')
     mu = 10000;
 end
@@ -39,14 +41,20 @@ Lambda = 0.1;
 nlcon = @(x) constraints(all_con, {}, x, 1);
 fmincon_options = optimoptions(@fmincon, 'Display', 'off', ...
                                'SpecifyObjectiveGradient', true);
-global xsol
-x_fmincon = fmincon(f, x0,[],[],[],[],[],[], nlcon, fmincon_options)
-fx_fmincon = f(x_fmincon)
-xsol = x_fmincon;
+
+x_fmincon = fmincon(f, x0,[],[],[],[],[],[], nlcon, fmincon_options);
+fx_fmincon = f(x_fmincon);
+
+counter.get_count()
+counter.reset_count()
+
 %%
 % [x, hs2] = l1_penalty(f, all_con, x0, mu, epsilon, delta, Lambda)
 [x, hs2] = l1_penalty_article(f, all_con, x0, mu, epsilon, delta, Lambda)
 fx = f(x)
+
+counter.get_count()
+counter.reset_count()
 
 
 tl1 = @() l1_penalty_article(f, all_con, x0, mu, epsilon, delta, Lambda);
