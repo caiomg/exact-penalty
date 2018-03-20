66e4a90ee7df5bf904bed8ac00791ada3cd1b933
Caio <caio@giuliani.eng.br>
2018-03-20 14:01:02 -0300

    Changes on accuracy of predict_descent

--- predict_descent.m
+++ predict_descent.m
@@ -1,15 +1,15 @@
-function pred = predict_descent(fmodel, con_model, p, mu, ind_eactive)
+function pred = predict_descent(fmodel, con_model, s, mu, ind_eactive)
 
 if nargin < 5
     ind_eactive = [];
 end
 
-f_change = (0.5*p'*fmodel.H*p + fmodel.g'*p);
+f_change = (0.5*(s'*fmodel.H*s) + fmodel.g'*s);
 c_change = 0;
 n_constraints = length(con_model);
 for k = 1:n_constraints
     if isempty(find(ind_eactive == k, 1))
-        this_change = (0.5*p'*con_model(k).H*p + con_model(k).g'*p);
+        this_change = (0.5*(s'*con_model(k).H*s) + con_model(k).g'*s);
         if con_model(k).c > 0
             if this_change + con_model(k).c < 0
                 this_change = -con_model(k).c;
