6669076eb0cd237911d8c2ab9dd769796f898cbc
Caio <caio@giuliani.eng.br>
2018-04-17 13:55:28 -0300

    Updated processing of results

--- process/check_run.m
+++ process/check_run.m
@@ -6,3 +6,14 @@ succeeded = false(n_problems, tries);
 for k = 1:tries
     succeeded(:, k) = check_run_single(all_results{k*n_problems});
 end
+
+
+b = sum(succeeded, 2);
+bi = [-max(0, min(1, b));
+      zeros(tries, 1)];
+c = ones(tries, 1);
+intcon = 1:tries;
+A = [-succeeded;
+     -eye(tries)];
+
+chosen = intlinprog(c, intcon, A, bi)
