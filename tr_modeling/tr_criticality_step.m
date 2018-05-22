499723f975c02ab6ac0e3257fb24e333949ec412
Caio <caio@giuliani.eng.br>
2018-05-22 07:41:07 -0300

    Corrected name of variable in criticality step

--- tr_modeling/tr_criticality_step.m
+++ tr_modeling/tr_criticality_step.m
@@ -1,11 +1,11 @@
-function [model, epsilon] = tr_criticality_step(model, f, epsilon, mu, ...
+function [model, epsilon] = tr_criticality_step(model, f, epsilon, p_mu, ...
                                      options)
 % CRITICALITY_STEP -- ensures model is sufficiently poised and with
 % a radius comparable to the gradient
 %
 
 
-mu = options.criticality_mu; % factor between radius and
+crit_mu = options.criticality_mu; % factor between radius and
                              % criticality measure
 omega = options.criticality_omega; % factor used to reduce radius
 beta = options.criticality_beta; % to ensure the final radius
@@ -19,14 +19,14 @@ model = improve_model(model, f, options);
 cmodel = extract_constraints_from_tr_model(model);
 [ind_eactive, ind_eviolated] = identify_new_constraints(cmodel, epsilon, []);
 [N, Q, R, ind_qr] = update_factorization(cmodel, [], [], ind_eactive, true);
-pseudo_gradient = l1_pseudo_gradient(f_grad, mu, cmodel, ind_eviolated);
+pseudo_gradient = l1_pseudo_gradient(f_grad, p_mu, cmodel, ind_eviolated);
 q1 = N'*pseudo_gradient;
 q2 = [cmodel(ind_qr).c]';
 
 measure = sqrt(q1'*q1 + q2'*q2);
 
 
-while (model.radius > mu*measure)
+while (model.radius > crit_mu*measure)
     model.radius = omega*model.radius;
     epsilon = 0.5*epsilon;
     
@@ -37,7 +37,7 @@ while (model.radius > mu*measure)
                                                       epsilon, []);
     [N, Q, R, ind_qr] = update_factorization(cmodel, [], [], ...
                                              ind_eactive, true);
-    pseudo_gradient = l1_pseudo_gradient(f_grad, mu, cmodel, ind_eviolated);
+    pseudo_gradient = l1_pseudo_gradient(f_grad, p_mu, cmodel, ind_eviolated);
     q1 = N'*pseudo_gradient;
     q2 = [cmodel(ind_qr).c]';
 
