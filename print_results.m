685422223ec94a59d1beb1b28d796957544452cf
Caio <caio@giuliani.eng.br>
2018-07-17 15:54:49 -0300

    Added lagrangian to print_results

--- print_results.m
+++ print_results.m
@@ -8,12 +8,14 @@ function print_results(results, fd)
         fprintf(fd, ' f(x): % +9.3g |', results.fx);
         fprintf(fd, ' evals: % 6d |', results.fcount);
         fprintf(fd, ' f err: % +9.3g |', results.error_obj);
+        fprintf(fd, ' g lag: % +9.3g |', norm(results.lgrad));
         fprintf(fd, ' viol.: % 9.3g |', results.nphi);
         fprintf(fd, ' %1d |', results.kkt);
     else
         fprintf(fd, ' f(x):           |');
         fprintf(fd, ' evals:        |');
         fprintf(fd, ' f err:           |');
+        fprintf(fd, ' g lag:           |');
         fprintf(fd, ' viol.:           |');
         fprintf(fd, ' 0 |');
     end
