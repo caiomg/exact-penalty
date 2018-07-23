698856e67304618e10827aea492f00c47d82b658
Caio <caio@giuliani.eng.br>
2018-07-23 10:31:43 -0300

    Drop constraint includes other active constraints

--- l1_drop_constraint.m
+++ l1_drop_constraint.m
@@ -1,5 +1,5 @@
-function [Q, R, N, ind_eactive] = ...
-    l1_drop_constraint(Q, R, N, ind_eactive, mu, multipliers, tol)
+function [Q, R, N, ind_qr, ind_eactive] = ...
+    l1_drop_constraint(cmodel, Q, R, ind_qr, ind_eactive, mu, multipliers, tol)
                                                        
 % I AM ASSUMING A = QR HAS THE SAME ORDENATION AS MULTIPLIERS!
 % Correct this in the future!
@@ -8,23 +8,25 @@ function [Q, R, N, ind_eactive] = ...
         tol = eps;
     end
 
-    rows_qr = size(Q, 2) - size(N, 2);
 
     % TODO: try other choices
     if min(multipliers) < tol && ~(-min(multipliers) < max(multipliers) - mu)
         [~, ind_j] = min(multipliers);
-        [Q, R] = qrdelete(Q, R, ind_j);
     elseif max(multipliers) > mu + tol
         [~, ind_j] = max(multipliers);
-        [Q, R] = qrdelete(Q, R, ind_j);
     end
 
     % Remove active constraint from set
-    ind_eactive(ind_j) = [];
-
-    % Update space orthogonal to active constraints
-    rows_qr = rows_qr - 1;
-    ind_null = (rows_qr+1):size(Q, 2);
-    N = Q(:, ind_null);
+    ind_eactive(ind_eactive == ind_qr(ind_j)) = [];
+    if length(ind_eactive) > length(ind_qr)
+        [N, Q, R, ind_qr] = update_factorization(cmodel, ...
+                                                 Q, R, ind_eactive, true);
+    else
+        ind_qr(ind_j) = [];
+        [Q, R] = qrdelete(Q, R, ind_j);
+        % Update space orthogonal to active constraints
+        r_columns = size(R, 2);
+        N = Q(:, r_columns+1:end);
+    end
 
 end
