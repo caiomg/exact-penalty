2426c9306bd9f5048f1a894932c281a783a9d849
Caio <caio@giuliani.eng.br>
2018-07-23 10:30:55 -0300

    Small change in calculation of pseudo-gradient

--- l1_pseudo_gradient.m
+++ l1_pseudo_gradient.m
@@ -1,4 +1,4 @@
-function dp = l1_pseudo_gradient(gfx, mu, cmodel, ind_eactive, dummy)
+function pg = l1_pseudo_gradient(gfx, mu, cmodel, ind_eactive, dummy)
 
 if nargin < 5
     error();
@@ -6,12 +6,12 @@ end
 
 n_constraints = length(cmodel);
 
-dp = 0;
+vg = 0;
 for n = 1:n_constraints
     if cmodel(n).c > 0 && ~sum(n == ind_eactive)
-        dp = dp + mu*(cmodel(n).g);
+        vg = vg + cmodel(n).g;
     end
 end
-dp = dp + gfx;
+pg = gfx + mu*vg;
 
 end
