ec2d7c4d72168deb7e2de7d08b254cf445ff3784
Caio <caio@giuliani.eng.br>
2018-09-24 12:48:25 -0300

    fixed bug on experiment

--- experiment_oogp.m
+++ experiment_oogp.m
@@ -34,23 +34,6 @@ x0_pos_gas = [14.13; 0.0502; 25.04; 0.0484; 23.8; 0.04; 35.89; 85.18];
 x0_more_co2 = [16.62; 0.0577;  29.46;  0.0557;  28.0;  0.046;  31.21; 74.07];
 x0_less_co2 = [16.62; 0.0427;  29.46;  0.0411;  28.0;  0.034;  31.21; 74.07];
 
-
-O = (bl + bu)/2;
-fixed_scale = (bu - bl)/2;
-no_scale = isinf(fixed_scale);
-fixed_scale(no_scale) = no_scale(no_scale);
-n_scale = sqrt(norm(fixed_scale));
-
-x0_scaled = (x0 - O)./fixed_scale;
-bl_scaled = (bl - O)./fixed_scale;
-bu_scaled = (bu - O)./fixed_scale;
-
-ec = evaluation_counter(@(y) santos_gas_network(y.*fixed_scale + O));
-cache = simple_cache(@(x) ec.evaluate(x), 3);
-
-% Objective: minimizing negative of oil production
-f = @(x) -cache.getvalue(x, 1);
-
 switch initial_condition
   case 'base'
     x0 = x_base;
@@ -79,6 +62,25 @@ switch scenario
   otherwise
     error('cmg:invalid_scenario', 'Invalid scenario choice');
 end
+
+
+
+O = (bl + bu)/2;
+fixed_scale = (bu - bl)/2;
+no_scale = isinf(fixed_scale);
+fixed_scale(no_scale) = no_scale(no_scale);
+n_scale = sqrt(norm(fixed_scale));
+
+x0_scaled = (x0 - O)./fixed_scale;
+bl_scaled = (bl - O)./fixed_scale;
+bu_scaled = (bu - O)./fixed_scale;
+
+ec = evaluation_counter(@(y) santos_gas_network(y.*fixed_scale + O));
+cache = simple_cache(@(x) ec.evaluate(x), 3);
+
+% Objective: minimizing negative of oil production
+f = @(x) -cache.getvalue(x, 1);
+
 
 all_con = {@(x) (cache.getvalue(x, 2) - gas_lim)/gas_lim;
           @(x) (cache.getvalue(x, 3) - co2_lim)/co2_lim};
