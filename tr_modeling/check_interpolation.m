5693c28745cec14b366598e0524b4049c40b0d1e
Caio <caio@giuliani.eng.br>
2018-11-09 09:13:41 -0200

    Simplified check_interpolation

--- tr_modeling/check_interpolation.m
+++ tr_modeling/check_interpolation.m
@@ -18,18 +18,23 @@ for m = n_points:-1:1
     h(:, m) = h(:, m) - model.points_abs(:, model.tr_center);
 end
 
-max_diff = -1;
 n_functions = size(model.fvalues, 1);
+max_diff = -ones(n_functions, 1);
+tol = zeros(n_functions, 1);
 for k = 1:n_functions
     [c, g, H] = get_model_matrices(model, k-1);
+    tol(k) = max(tol_1*max(abs(model.fvalues(k, :))), tol_2);
     for m = 1:n_points
         this_value = c + g'*h(:, m) + 0.5*(h(:, m)'*H*h(:, m));
         difference = abs(model.fvalues(k, m) - this_value);
-        if difference > max_diff
-            max_diff = difference;
-        end
-        if abs(difference) > max(tol_1*max(abs(model.fvalues(k, :))), tol_2)
-            warning('cmg:tr_interpolation_error', 'Interpolation error');
+        if difference > max_diff(k)
+            max_diff(k) = difference;
         end
     end
 end
+violated = (max_diff > tol);
+max_diff = max(max_diff);
+if ~isempty(find(violated, 1))
+   warning('cmg:tr_interpolation_error', 'Interpolation error');
+end
+
