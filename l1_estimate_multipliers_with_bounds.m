df9dd3c2cd473f019295eddb00a82280aedc6dc6
Caio <caio@giuliani.eng.br>
2019-07-06 13:57:48 -0300

    New initial estimate for estimation of multipliers

--- l1_estimate_multipliers_with_bounds.m
+++ l1_estimate_multipliers_with_bounds.m
@@ -3,17 +3,21 @@ function [con_multipliers, lb_multipliers, ub_multipliers, tol] = ...
 % L1_ESTIMATE_MULTIPLIERS_NO_BOUNDS - 
 %   
 
+    n_lower_active = sum(lb_active);
+    n_upper_active = sum(ub_active);
+    n_bounds_active = n_lower_active + n_upper_active;
+    n_cons_total = size(R, 2);
+    n_cons_not_bounds = n_cons_total - n_bounds_active;
+
+    initial_estimate = zeros(n_cons_total, 1);
+    [initial_estimate, tol] = l1_estimate_multipliers_no_bounds(p_grad, ...
+                                                      Q, R);
     dim = size(p_grad, 1);
     H = R'*R;
     g = (R'*Q'*p_grad);
     
     f = @(x) quadratic(H, g, 0, x);
     
-    n_lower_active = sum(lb_active);
-    n_upper_active = sum(ub_active);
-    n_bounds_active = n_lower_active + n_upper_active;
-    n_cons_total = size(H, 2);
-    n_cons_not_bounds = n_cons_total - n_bounds_active;
 
     bl_mult = [-inf(n_cons_not_bounds, 1);
                zeros(n_bounds_active, 1)];
@@ -25,7 +29,7 @@ function [con_multipliers, lb_multipliers, ub_multipliers, tol] = ...
                                    'OptimalityTolerance', 1e-6);
 
     [all_multipliers, ~, exitflag, ~, fmincon_lambda] = ...
-        fmincon(f, zeros(size(g)), [], [], [], [], bl_mult, [], [], fmincon_options);
+        fmincon(f, initial_estimate, [], [], [], [], bl_mult, [], [], fmincon_options);
     
     con_multipliers = all_multipliers(1:n_cons_not_bounds);
     lb_multipliers = zeros(dim, 1);
@@ -35,7 +39,7 @@ function [con_multipliers, lb_multipliers, ub_multipliers, tol] = ...
                                      n_cons_not_bounds+ n_lower_active);
     ub_multipliers(ub_active) = all_multipliers(n_cons_not_bounds+ n_lower_active+1:end);
     
-    assert(norm(all_multipliers(fmincon_lambda.lower > 0), inf) == 0);
+%     assert(norm(all_multipliers(fmincon_lambda.lower > 0), inf) == 0);
 
     tol = sqrt(eps);
 
