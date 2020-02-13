669b347cb8eafca611292dd7e350be9117e7bac0
Caio Giuliani <caio@giuliani.eng.br>
2020-02-13 09:59:56 -0300

    Cleanup. Deleted some files

--- format_result.m
+++ format_result.m
@@ -5,14 +5,14 @@ function s = format_result(result)
     s = [s, sprintf(' %8s |', result.name)];
     if ~isempty(result.fx)
         s = [s, sprintf(' fx: % +9.3g |', result.fx)];
-        s = [s, sprintf(' #f: % 6d |', result.fcount)];
+        s = [s, sprintf(' #f: % 5d |', result.fcount)];
         s = [s, sprintf(' err: % +9.3g |', result.error_obj)];
         s = [s, sprintf(' g lag: % +9.3g |', norm(result.lgrad))];
         s = [s, sprintf(' viol: % 9.3g |', result.nphi)];
         s = [s, sprintf(' %1d |', result.kkt)];
     else
         s = [s, sprintf(' fx:           |')];
-        s = [s, sprintf(' #f:        |')];
+        s = [s, sprintf(' #f:       |')];
         s = [s, sprintf(' err:           |')];
         s = [s, sprintf(' g lag:           |')];
         s = [s, sprintf(' viol:           |')];
