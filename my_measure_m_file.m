714b88dc8e18f8b400d85edd00c9b0793c9f3c84
Caio Giuliani <caio@giuliani.eng.br>
2020-03-13 09:44:24 -0300

    minor adjustment

--- my_measure_m_file.m
+++ my_measure_m_file.m
@@ -2,7 +2,7 @@ function measure = my_measure_m_file(params)
 % MY_MEASURE_M_FILE - 
 %   
 
-    p_indices = (19:30)';
+    p_indices = (16:27)';
     tmax = 1e4;
     measure = profile_integral(harder_problems(params, p_indices), ...
                                p_indices, tmax)/tmax;
